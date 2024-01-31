8a156d1f0acb4a74557802ffda76b59887f0017c
Harsh dubey <95978233+mrharshpandit@users.noreply.github.com>
2024-01-31 10:11:28 +0530

    Add files via upload

---
+++ Harsh Dubey Resume/index.js
@@ -0,0 +1,12 @@
+// Get all elements with class "h"
+var hElements = document.getElementsByClassName("h");
+
+// Iterate through the elements and change the color to red
+for (var i = 0; i < hElements.length; i++) {
+    hElements[i].style.color = "red";
+}
+
+// setTimeout(function(){
+//     alert("dont forget to hire me!");
+// },6000);
+
